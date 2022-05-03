b455c32a3a9b25bed09a8126f94737ce8cfbbd52
Boycapel <boycapel@outlook.com>
2022-05-03 07:31:01 -0400

    Customizes portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,6 +11,16 @@ const Projects = () => {
             <div className="projects__cards">
 
                 <div className="projects__card">
+                    <h3>NFT Minting Bot</h3>
+                    <img src={mintingbot} alt="NFT Minting Bot" />
+                    <p>a  NFT Batch minting bot  that can mint one or multiple nfts.
+                    </p>
+
+                   
+                    <a href="https://github.com/Boycapel/nftmintingbot" target="_blank" className="button">Code</a>
+                </div>
+
+                <div className="projects__card">
                     <h3>Uniswap</h3>
                     <img src={uniswap} alt="Uniswap Swap Page" />
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -24,24 +34,22 @@ const Projects = () => {
 
                 <div className="projects__card">
                     <h3>Defi Yield Aggregator</h3>
-                    <img src={compound} alt="Defi Yield Aggregator" />
+                    <img src={ya} alt="Defi Yield Aggregator" />
                     <p>This project is a simple decentrialized app where a user can deposit DAI into our smart contract. Once funds are deposited, the contract compares the interest rate of Compound & Aave, and deposits funds to whichever has the highest interest rate. The user can rebalance his/her funds to ensure that the funds are still currently in the higher interest rate protocol, and can also withdraw at any time.
                     </p>
 
-                    <a href="https://compound.finance/" target="_blank" className="button">Site</a>
+                
                     <a href="https://github.com/Boycapel/YA" target="_blank" className="button">Code</a>
                 </div>
 
                 <div className="projects__card">
-                    <h3>Aave</h3>
-                    <img src={aave} alt="Aave Landing Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <h3>NFT Marketplace</h3>
+                    <img src={nftmarketplace} alt="NFT Marketplace"/>
+                    <p>This project I cloned and created a NFT marketplace that mimics the functions of OpenSea, and catalogged all transactions using metamask, and webiste.
                     </p>
 
-                    <a href="https://aave.com/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/aave/aave-v3-core" target="_blank" className="button">Code</a>
+                    
+                    <a href="https://github.com/Boycapel/openseaCLONE" target="_blank" className="button">Code</a>
                 </div>
             </div>
         </section>
